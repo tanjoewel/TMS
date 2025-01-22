be2528f5e6e64fa00c8f4c19d0edfd4295a7dd8f
tanjoewel <joeweltan99@gmail.com>
2025-01-22 15:02:56 +0800

    feat: set up login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const Home = () => {
+  const LoginButton = styled(Button)({
+    backgroundColor: "blue",
+    fontSize: 16,
+    border: "1px grey solid",
+    color: "white",
+    textTransform: "none",
+  });
+
+  function handleClick() {
+    // TODO: submit the request to the backend here. Remember to hash the password before sending it to the backend. Do once backend is set up.
+    console.log("Button clicked");
+  }
+
+  // TODO: client-side error validation. Do once backend is set up.
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
-      <div>Body of the page</div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "80vh",
+          gap: 2,
+          textAlign: "center",
+          minHeight: "300px",
+        }}
+      >
+        <TextField
+          id="username"
+          label="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <LoginButton onClick={handleClick} sx={{ width: "100px" }}>
+          Login
+        </LoginButton>
+      </Box>
     </div>
   );
 };
