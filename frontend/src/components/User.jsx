27dfc0fe825eea90a64d5b07cd8e9ad92c6a4d64
tanjoewel <joeweltan99@gmail.com>
2025-01-22 17:35:23 +0800

    feat: set up group name and create button

---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// this component represents one user and its fields, then in the users page we can just loop through the users array we get from the backend and return this component in each loop.
+const User = () => {
+  return <div>User</div>;
+};
+
+export default User;
--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { TextField, Box, Button } from "@mui/material";
 
 export default function Users() {
+  const [groupname, setGroupname] = useState("");
+
   return (
     <div>
       <nav>
         <NavLink to="/">Back to login</NavLink>
       </nav>
-      <h2>Users</h2>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", px: 5 }}>
+        <h2>Users</h2>
+        <div>
+          <TextField
+            id="groupname"
+            label="Group name"
+            size="small"
+            onChange={(e) => {
+              setGroupname(e.target.value);
+            }}
+          />
+          <Button>Create</Button>
+        </div>
+      </Box>
     </div>
   );
 }
+
+("display: flex; justify-content: space-between; align-items: center; width:100% ");
