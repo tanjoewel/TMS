5de6daa841f8cc0f47015330a3e1de3cf368a536
tanjoewel <joeweltan99@gmail.com>
2025-01-22 13:04:26 +0800

    chore: apply prettier styling to all component and page files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Users from './pages/Users';
-
+import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Users from "./pages/Users";
+import Header from "./components/Header";
 
 function App() {
-
   return (
+    <Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<Users />} />
       </Routes>
+    </Header>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  
   return (
     <div>
       <h1>Task Management System</h1>
