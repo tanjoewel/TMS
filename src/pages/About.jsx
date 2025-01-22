a3ee6b4784c21384814e3683c9d84e52f5c3be0f
tanjoewel <joeweltan99@gmail.com>
2025-01-22 14:26:22 +0800

    fix: remove unnecessary component LoginBody

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,11 @@ export default function About() {
   return (
     <div>
       <h2>About</h2>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/users">Users</NavLink>
+      </nav>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import HomeBody from "../components/LoginBody";
 
 const Home = () => {
   return (
     <div>
-      <HomeBody />
       <div>Body of the page</div>
     </div>
   );
