01487532836f69e9b1eecda9e012dfa7c1fd0381
tanjoewel <joeweltan99@gmail.com>
2025-01-23 10:32:04 +0800

    feat: set up typical express backend folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { pool, executeQuery } = require("./sql");
+const { executeQuery } = require("./util/sql");
 
 const app = express();
 
