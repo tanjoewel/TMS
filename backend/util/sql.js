6ee93609bcebb4ac3fc856bf2a0f5ef627abe3df
tanjoewel <joeweltan99@gmail.com>
2025-01-24 08:57:12 +0800

    feat: throw error when querying so functions using it can catch the error

--- backend/util/sql.js
+++ backend/util/sql.js
@@ -16,7 +16,9 @@ async function executeQuery(query, args) {
     const [result] = await pool.query(query, args);
     return result;
   } catch (err) {
-    console.error("Error when querying: ", err.message);
+    // console.error("Error when querying: ", err.message);
+    // propagate it up so that other functions can catch it and return the appropriate errors to the user
+    throw new Error(`Error when querying: ${err.message}`);
   }
 
   // closing the connection is causing errors because after the first query the connection is closed and cannot accept any more queries.
