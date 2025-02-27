8a7e27363239e2b625257ec3bb9a3c24108c60d8
tanjoewel <joeweltan99@gmail.com>
2025-02-27 15:38:10 +0800

    feat: add database connection and test it

--- assignment-3-backend/controllers/a3controller.js
+++ assignment-3-backend/controllers/a3controller.js
@@ -1,9 +1,18 @@
+const db = require("../util/db");
+
 exports.createTask = async function (req, res) {
   res.send("Create task");
 };
 
 exports.getTaskbyState = async function (req, res) {
-  res.send("get task by state");
+  const connection = await db.getConnection();
+  const query = "SELECT * FROM task";
+  try {
+    const [result] = await connection.execute(query);
+    res.send(result);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get tasks" });
+  }
 };
 
 exports.promoteTask2Done = async function (req, res) {
