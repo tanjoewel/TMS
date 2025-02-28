df2102c21ee674ba324c37300cdeaa5758a382f7
tanjoewel <joeweltan99@gmail.com>
2025-02-28 10:10:02 +0800

    feat: invalid url error

--- assignment-3-backend/routes/a3routes.js
+++ assignment-3-backend/routes/a3routes.js
@@ -6,8 +6,8 @@ const a3controller = require("../controllers/a3controller");
 router.post("/CreateTask", a3controller.createTask);
 router.post("/GetTaskbyState", a3controller.getTaskbyState);
 router.patch("/PromoteTask2Done", a3controller.promoteTask2Done);
-// router.get("/*", (req, res) => {
-//   res.status(404).send("Invalid URL");
-// })
+router.all("/*", (req, res) => {
+  res.status(400).json({ code: "E1001" });
+});
 
 module.exports = router;
